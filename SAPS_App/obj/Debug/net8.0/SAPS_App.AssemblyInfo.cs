57a66caf2a2c36c756b216a09248e91c8ee97231
EviweKhohlombe <eviwek@joburg.org.za>
2025-01-24 10:54:08 +0200

    Updated AssinRoleModel to AssignRoleModel

--- SAPS_App/obj/Debug/net8.0/SAPS_App.AssemblyInfo.cs
+++ SAPS_App/obj/Debug/net8.0/SAPS_App.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAPS_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3ca79e265bcfc159bef22d1f7e23375eb8b52ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79c45f9c0f2ccf62b89206f12b71ef3cde930cfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAPS_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAPS_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
